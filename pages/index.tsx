957e16e7fdee37efad6676eac2cc20ed1b1d17fd
jodie.walker <jodie.walker@jokercreative.com>
2022-08-11 21:35:05 +1000

    inital api call setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,42 @@
-export default function Home() {
-  return (
-    <div>
+import { useEffect, useState } from "react"
 
-    </div>
-  )
+export default function Home() {
+
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(`/api/images?page=${page}`)
+    .then(res => res.json())
+    .then(data => {
+      setLoading(false);
+      setData(data);
+    })
+  }, [page]);
+
+  const loadMore = () => {
+    setPage(page+1)
+  }
+
+  if(isLoading) {
+    return (
+      <p>loading</p>
+    )
+  } else {
+    return (
+      <div>
+        {data && data.length > 0 &&
+          <div>
+            {data.map((item, i) => {
+              return (<p>{item.id}</p>)
+            })}
+          </div>
+        }
+
+        <button onClick={loadMore}>load more</button>
+      </div>
+    )
+  }
 }
