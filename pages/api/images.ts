58676b7526e875858f12907a2e2200555ceabc7f
jodie.walker <jodie.walker@jokercreative.com>
2022-08-12 14:05:41 +1000

    changing api to search

--- pages/api/images.ts
+++ pages/api/images.ts
@@ -1,9 +1,9 @@
 // add API_KEY into .env file
 export default async function handler(req, res) {
-  const query = req.query;
-  const { page } = query;
-  const resData = await fetch(`https://api.unsplash.com/photos/?client_id=${process.env.API_KEY}&page=${page}`)
+  const searchQuery = req.query;
+  const { page, query } = searchQuery;
+  const resData = await fetch(`https://api.unsplash.com/search/photos/?client_id=${process.env.API_KEY}&page=${page}&query=${query}`)
     .then(response => response.json());
 
-  res.status(200).json(resData);
+  res.status(200).json(resData.results);
 }
